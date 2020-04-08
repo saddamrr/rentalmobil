fab9388fcf8ae31a84f179e32b32fbb2bfb7ab73
saddam_raihan_ramadhan <saddam_raihan_27rpl@student.smktelkom-mlg.sch.id>
2020-04-08 07:51:19 +0700

    auth

---
+++ app/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table = 'petugas';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'nama_petugas', 'username', 'password', 'level', 'no_telp',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public $timestamps = false;
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
